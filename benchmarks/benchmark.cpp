20cb71b06d84889e061c24ac568424a0c458a5c8
blegouix <stilynx51@gmail.com>
2025-06-03 17:40:24 +0200

    add mdspan in dependency and first usage

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -2,11 +2,30 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include <batched_reduction_performance/batched_reduction_performance.hpp>
+#include <iostream>
+
+#include <mdspan/mdspan.hpp>
 
 #include <benchmark/benchmark.h>
 
+#include <batched_reduction_performance/batched_reduction_performance.hpp>
+
+static constexpr std::size_t N = 1024;
+static constexpr std::size_t M = 32;
+
 void dummy_benchmark(benchmark::State &state) {
+  std::array<double, N * M> mat_alloc;
+
+  Kokkos::mdspan<double, Kokkos::extents<std::size_t, N, M>> data(
+      mat_alloc.data(), N, M);
+  for (std::size_t i = 0; i < data.extent(0); ++i) {
+    for (std::size_t j = 0; j < data.extent(1); ++j) {
+      data(i, j) = i * N + j;
+      // std::cout << data(i, j) << " ";
+    }
+    // std::cout << "\n";
+  }
+
   for (auto _ : state) {
     batched_reduction_kernel::dummy_kernel();
   }
