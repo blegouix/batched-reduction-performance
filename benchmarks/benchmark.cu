76a122affa62b5d8511a3b9b612762c666a1858e
blegouix <stilynx51@gmail.com>
2025-06-04 11:02:26 +0200

    wip on sequential_reduction

--- benchmarks/benchmark.cu
+++ benchmarks/benchmark.cu
@@ -14,20 +14,28 @@ static constexpr std::size_t M = 32;
 static constexpr std::size_t N = 1024;
 
 void dummy_benchmark(benchmark::State &state) {
-  double *mat_ptr = nullptr;
-  cudaMalloc(&mat_ptr, M * N * sizeof(double));
+  double *data_in_ptr = nullptr;
+  cudaMalloc(&data_in_ptr, M * N * sizeof(double));
 
-  cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>> mat(mat_ptr);
-  filler::fill(mat);
-  printer::print(mat);
+  cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>> data_in(
+      data_in_ptr);
+  filler::fill(data_in);
+  // printer::print(mat);
+
+  double *data_out_ptr = nullptr;
+  cudaMalloc(&data_out_ptr, N * sizeof(double));
+
+  cuda::std::mdspan<double, cuda::std::extents<std::size_t, N>> data_out(
+      data_out_ptr);
 
   for (auto _ : state) {
-    batched_reduction_kernel::dummy_kernel();
+    batched_reduction_kernel::Sequential::run(data_out, data_in);
   }
   state.SetBytesProcessed(int64_t(state.iterations()) *
                           int64_t(state.range(0) * sizeof(double)));
 
-  cudaFree(mat_ptr);
+  cudaFree(data_in_ptr);
+  cudaFree(data_out_ptr);
 }
 
 std::size_t constexpr dummy_param = 128;
--- include/batched_reduction_performance/batched_reduction_kernel/batched_reduction_kernel.hpp
+++ include/batched_reduction_performance/batched_reduction_kernel/batched_reduction_kernel.hpp
@@ -1,10 +1,49 @@
 // SPDX-FileCopyrightText: 2024 Baptiste Legouix
 // SPDX-License-Identifier: MIT
 
+#include <cuda/std/mdspan>
+
 #pragma once
 
 namespace batched_reduction_kernel {
 
-void dummy_kernel() { return; }
+namespace detail {
+
+template <std::size_t _M, std::size_t _N>
+static __global__ void sequential_kernel(
+    cuda::std::mdspan<double, cuda::std::extents<std::size_t, _N>> data_out,
+    cuda::std::mdspan<double, cuda::std::extents<std::size_t, _M, _N>>
+        data_in) {
+  std::size_t i = blockIdx.x * blockDim.x + threadIdx.x;
+
+  // if (i < _N && j < _N) {
+  double tmp = 0;
+  // TOOD shared buffer for data_in[i]
+
+  for (std::size_t j = 0; j < _M; ++j) {
+    tmp += data_in(i, j);
+  }
+
+  data_out(i) = tmp;
+  printf("%f ", data_out(i));
+  // }
+}
+
+} // namespace detail
+
+class Sequential {
+public:
+  template <std::size_t _M, std::size_t _N>
+  static void
+  run(cuda::std::mdspan<double, cuda::std::extents<std::size_t, _N>> data_out,
+      cuda::std::mdspan<double, cuda::std::extents<std::size_t, _M, _N>>
+          data_in) {
+    dim3 blockDim(256);
+    dim3 gridDim((_N + blockDim.x - 1) / blockDim.x);
+
+    detail::sequential_kernel<<<gridDim, blockDim>>>(data_out, data_in);
+    cudaDeviceSynchronize();
+  }
+};
 
 } // namespace batched_reduction_kernel
