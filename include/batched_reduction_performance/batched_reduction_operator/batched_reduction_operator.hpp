0c15b6a330c9bfc58ceae72b9034e569c5b36a41
blegouix <stilynx51@gmail.com>
2025-06-04 14:51:10 +0200

    wip

--- include/batched_reduction_performance/batched_reduction_operator/batched_reduction_operator.hpp
+++ include/batched_reduction_performance/batched_reduction_operator/batched_reduction_operator.hpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Baptiste Legouix
 // SPDX-License-Identifier: MIT
 
+#include <cooperative_groups.h>
 #include <cuda/std/mdspan>
 
 #pragma once
@@ -24,13 +25,13 @@ static __global__ void sequential_kernel(
     static_assert(M % 32 == 0, "Uncomplete warps are not allowed, fix the "
                                "problem sizes or enable ALLOW_UNCOMPLETE_WARP");
 #endif
-    double tmp = 0;
+    double sum = 0;
 
     for (std::size_t j = 0; j < N; ++j) {
-      tmp += data_in(i, j);
+      sum += data_in(i, j);
     }
 
-    data_out(i) = tmp;
+    data_out(i) = sum;
   }
 }
 
@@ -50,56 +51,65 @@ public:
   }
 };
 
-// Sequential operator with shared memory usage
+// Parallel operator using CUDA cooperative groups
 
 namespace detail {
 
 template <std::size_t M, std::size_t N>
-static __global__ void sequential_with_shared_memory_kernel(
+static __global__ void cooperative_groups_kernel(
     cuda::std::mdspan<double, cuda::std::extents<std::size_t, M>> data_out,
     cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>> data_in) {
   std::size_t i = blockIdx.x * blockDim.x + threadIdx.x;
+  std::size_t j = blockIdx.y * blockDim.y + threadIdx.y;
 
+  double val;
 #if defined ALLOW_UNCOMPLETE_WARP
-  if (i < M) {
+  if (i < M && j < N) {
 #else
   {
-    static_assert(M % 32 == 0, "Uncomplete warps are not allowed, fix the "
-                               "problem sizes or enable ALLOW_UNCOMPLETE_WARP");
+    static_assert(M % 32 == 0 && N % 32 == 0,
+                  "Uncomplete warps are not allowed, fix the problem sizes or "
+                  "enable ALLOW_UNCOMPLETE_WARP");
 #endif
+    val = data_in(i, j);
+  }
+#if defined ALLOW_UNCOMPLETE_WARP
+  else {
+#else
+  {
+    static_assert(M % 32 == 0 && N % 32 == 0,
+                  "Uncomplete warps are not allowed, fix the problem sizes or "
+                  "enable ALLOW_UNCOMPLETE_WARP");
+#endif
+    val = 0.;
+  }
 
-    // Shared memory buffer storing one row of data_in
-    __shared__ double buffer[N];
-    for (std::size_t j = 0; j < N; ++j) {
-      buffer[j] = data_in(i, j);
-    }
+  // Perform reduction within the block
+  double sum =
+      cooperative_groups::reduce(cooperative_groups::this_thread_block(), val,
+                                 cooperative_groups::plus<double>());
 
-    __syncthreads();
-
-    double tmp = 0;
-
-    for (std::size_t j = 0; j < N; ++j) {
-      tmp += data_in(i, j);
-    }
-
-    data_out(i) = tmp;
+  // Thread 0 writes result to output
+  if (cooperative_groups::this_thread_block().thread_rank() == 0) {
+    data_out(i) = sum;
   }
 }
 
 } // namespace detail
 
-template <std::size_t BlockDim> class SequentialWithSharedMemory {
+template <std::size_t BlockDim1, std::size_t BlockDim2>
+class SequentialWithSharedMemory {
 public:
   template <std::size_t M, std::size_t N>
   static void
   run(cuda::std::mdspan<double, cuda::std::extents<std::size_t, M>> data_out,
       cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>>
           data_in) {
-    dim3 const blockDim(BlockDim);
-    dim3 const gridDim((M + blockDim.x - 1) / blockDim.x);
+    dim3 const blockDim(BlockDim1, BlockDim2);
+    dim3 const gridDim((M + blockDim.x - 1) / blockDim.x,
+                       (N + blockDim.y - 1) / blockDim.y);
 
-    detail::sequential_with_shared_memory_kernel<<<gridDim, blockDim>>>(
-        data_out, data_in);
+    detail::cooperative_groups_kernel<<<gridDim, blockDim>>>(data_out, data_in);
   }
 };
 
