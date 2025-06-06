eacc05feb246a0eec36263182799eaf9c8181a2d
blegouix <stilynx51@gmail.com>
2025-06-06 10:21:20 +0200

    support both layout stride

--- benchmarks/benchmark.cu
+++ benchmarks/benchmark.cu
@@ -14,7 +14,7 @@ static constexpr std::size_t BlockDim2D_1 = 4;
 static constexpr std::size_t BlockDim2D_2 = 4;
 
 template <std::size_t M, std::size_t N, class BatchedReductionOperator,
-          class Layout>
+          wrapper::LayoutEnum Layout>
 class BatchedReductionBenchmark {
   static_assert(M >= BlockDim1D &&
                 "M has to be equal or greater than BlockDim1D");
@@ -29,7 +29,8 @@ public:
     double *data_in_ptr = nullptr;
     cudaMalloc(&data_in_ptr, M * N * sizeof(double));
 
-    cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>, Layout>
+    cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>,
+                      wrapper::mdspan_layout_t<Layout>>
         data_in = wrapper::wrap<M, N, Layout>(data_in_ptr);
     filler::fill<BlockDim2D_1, BlockDim2D_2>(data_in);
     // printer::print<BlockDim2D_1, BlockDim2D_2>(data_in);
@@ -57,57 +58,74 @@ public:
 #define BENCHMARKS(M, N)                                                         \
   BENCHMARK(BatchedReductionBenchmark<                                           \
             M, N, batched_reduction_operator::Sequential<BlockDim1D>,            \
-            cuda::std::layout_right>::run);                                      \
+            wrapper::LayoutEnum::Right>::run);                                   \
   BENCHMARK(BatchedReductionBenchmark<                                           \
             M, N, batched_reduction_operator::Sequential<BlockDim1D>,            \
-            cuda::std::layout_left>::run);                                       \
+            wrapper::LayoutEnum::Left>::run);                                    \
   BENCHMARK(BatchedReductionBenchmark<                                           \
             M, N, batched_reduction_operator::Sequential<BlockDim1D>,            \
-            cuda::std::layout_stride>::run);                                     \
+            wrapper::LayoutEnum::RightStride>::run);                             \
+  BENCHMARK(BatchedReductionBenchmark<                                           \
+            M, N, batched_reduction_operator::Sequential<BlockDim1D>,            \
+            wrapper::LayoutEnum::LeftStride>::run);                              \
   /*                                                                             \
     BENCHMARK(                                                                   \
         BatchedReductionBenchmark<M, N,                                          \
                                   batched_reduction_operator::CooperativeGroups, \
-                                  cuda::std::layout_right>::run);                \
+                                  wrapper::LayoutEnum::Right>::run);             \
     BENCHMARK(                                                                   \
         BatchedReductionBenchmark<M, N,                                          \
                                   batched_reduction_operator::CooperativeGroups, \
-                                  cuda::std::layout_left>::run);                 \
+                                  wrapper::LayoutEnum::Left>::run);              \
     BENCHMARK(                                                                   \
         BatchedReductionBenchmark<M, N,                                          \
                                   batched_reduction_operator::CooperativeGroups, \
-                                  cuda::std::layout_stride>::run);               \
+                                  wrapper::LayoutEnum::RightStride>::run);       \
+    BENCHMARK(                                                                   \
+        BatchedReductionBenchmark<M, N,                                          \
+                                  batched_reduction_operator::CooperativeGroups, \
+                                  wrapper::LayoutEnum::LeftStride>::run);        \
   */                                                                             \
   BENCHMARK(                                                                     \
       BatchedReductionBenchmark<M, N,                                            \
                                 batched_reduction_operator::CUBBlockReduction<   \
                                     cub::BLOCK_REDUCE_WARP_REDUCTIONS>,          \
-                                cuda::std::layout_right>::run);                  \
+                                wrapper::LayoutEnum::Right>::run);               \
   BENCHMARK(                                                                     \
       BatchedReductionBenchmark<M, N,                                            \
                                 batched_reduction_operator::CUBBlockReduction<   \
                                     cub::BLOCK_REDUCE_WARP_REDUCTIONS>,          \
-                                cuda::std::layout_left>::run);                   \
+                                wrapper::LayoutEnum::Left>::run);                \
   BENCHMARK(                                                                     \
       BatchedReductionBenchmark<M, N,                                            \
                                 batched_reduction_operator::CUBBlockReduction<   \
                                     cub::BLOCK_REDUCE_WARP_REDUCTIONS>,          \
-                                cuda::std::layout_stride>::run);                 \
+                                wrapper::LayoutEnum::RightStride>::run);         \
+  BENCHMARK(                                                                     \
+      BatchedReductionBenchmark<M, N,                                            \
+                                batched_reduction_operator::CUBBlockReduction<   \
+                                    cub::BLOCK_REDUCE_WARP_REDUCTIONS>,          \
+                                wrapper::LayoutEnum::LeftStride>::run);          \
   BENCHMARK(BatchedReductionBenchmark<                                           \
             M, N,                                                                \
             batched_reduction_operator::CUBBlockReduction<                       \
                 cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY>,                      \
-            cuda::std::layout_right>::run);                                      \
+            wrapper::LayoutEnum::Right>::run);                                   \
   BENCHMARK(BatchedReductionBenchmark<                                           \
             M, N,                                                                \
             batched_reduction_operator::CUBBlockReduction<                       \
                 cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY>,                      \
-            cuda::std::layout_left>::run);                                       \
+            wrapper::LayoutEnum::Left>::run);                                    \
   BENCHMARK(BatchedReductionBenchmark<                                           \
             M, N,                                                                \
             batched_reduction_operator::CUBBlockReduction<                       \
                 cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY>,                      \
-            cuda::std::layout_stride>::run);
+            wrapper::LayoutEnum::RightStride>::run);                             \
+  BENCHMARK(BatchedReductionBenchmark<                                           \
+            M, N,                                                                \
+            batched_reduction_operator::CUBBlockReduction<                       \
+                cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY>,                      \
+            wrapper::LayoutEnum::LeftStride>::run);
 
 /*
 BENCHMARKS(65536, 64);
--- include/batched_reduction_performance/wrapper/wrapper.hpp
+++ include/batched_reduction_performance/wrapper/wrapper.hpp
@@ -13,19 +13,45 @@ static constexpr std::size_t stride =
     32; // Attempt to optimize benchmarks using layout_stride according to warp
         // size
 
+enum LayoutEnum { Left, LeftStride, RightStride, Right };
+
 namespace detail {
 
-template <std::size_t M, std::size_t N, class Layout> struct MakeMDSpan {
+template <LayoutEnum Layout> struct MDSpanLayout;
+
+template <> struct MDSpanLayout<Left> {
+  using type = cuda::std::layout_left;
+};
+
+template <> struct MDSpanLayout<LeftStride> {
+  using type = cuda::std::layout_stride;
+};
+
+template <> struct MDSpanLayout<RightStride> {
+  using type = cuda::std::layout_stride;
+};
+
+template <> struct MDSpanLayout<Right> {
+  using type = cuda::std::layout_right;
+};
+
+} // namespace detail
+
+template <LayoutEnum Layout>
+using mdspan_layout_t = typename detail::MDSpanLayout<Layout>::type;
+
+namespace detail {
+
+template <std::size_t M, std::size_t N, LayoutEnum Layout> struct MakeMDSpan {
   static cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>,
-                           Layout>
+                           mdspan_layout_t<Layout>>
   run(double *ptr) {
     return cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>,
-                             Layout>(ptr);
+                             mdspan_layout_t<Layout>>(ptr);
   }
 };
 
-template <std::size_t M, std::size_t N>
-struct MakeMDSpan<M, N, cuda::std::layout_stride> {
+template <std::size_t M, std::size_t N> struct MakeMDSpan<M, N, LeftStride> {
   static cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>,
                            cuda::std::layout_stride>
   run(double *ptr) {
@@ -38,11 +64,24 @@ struct MakeMDSpan<M, N, cuda::std::layout_stride> {
   }
 };
 
+template <std::size_t M, std::size_t N> struct MakeMDSpan<M, N, RightStride> {
+  static cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>,
+                           cuda::std::layout_stride>
+  run(double *ptr) {
+    return cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>,
+                             cuda::std::layout_stride>(
+        ptr, cuda::std::layout_stride::mapping<
+                 cuda::std::extents<std::size_t, M, N>>{
+                 cuda::std::extents<std::size_t, M, N>{},
+                 cuda::std::array<std::size_t, 2>{stride, 1}});
+  }
+};
+
 } // namespace detail
 
-template <std::size_t M, std::size_t N, class Layout>
+template <std::size_t M, std::size_t N, LayoutEnum Layout>
 cuda::std::mdspan<double, cuda::std::extents<std::size_t, M, N>,
-                  cuda::std::layout_stride>
+                  mdspan_layout_t<Layout>>
 wrap(double *ptr) {
   return detail::MakeMDSpan<M, N, Layout>::run(ptr);
 }
